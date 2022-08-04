7b3fd6a7cfec1dff9c7bb254e4cc3038e23ef3a0
Sophia Wall <sophiapwall@yahoo.co.uk>
2022-08-04 14:17:50 +0100

    api friends

---
+++ src/routes/api/friends.js
@@ -0,0 +1,8 @@
+const router = require("express").Router();
+const { Thought, User } = require("../../models");
+
+//POST /friends/:friendId
+router.post("/friends/:friendId", async (req, res) => {});
+
+//DEL /friends/:friendId
+router.delete("/friends/:friendId", async (req, res) => {});
