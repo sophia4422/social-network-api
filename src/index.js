0d76037a18cfece60af4136123fe3ad028e75815
Sophia Wall <sophiapwall@yahoo.co.uk>
2022-08-10 19:02:34 +0100

    dotenv

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-const express = require("express");
+require("dotenv").config();
 
+const express = require("express");
+const mongoose = require("mongoose");
 const routes = require("./routes");
 
 // const apiRoutes = require("./routes/api");
