6d294c3eb5675b981cc11be9ac76f871bdb118c7
Sophia Wall <sophiapwall@yahoo.co.uk>
2022-08-10 19:48:48 +0100

    users done

--- src/controllers/api/reactions.js
+++ src/controllers/api/reactions.js
@@ -3,3 +3,5 @@ const { User, Thought } = require("../../models");
 const createReactionForThought = (req, res) => {};
 
 const removeReactionFromThought = (req, res) => {};
+
+module.exports = { createReactionForThought, removeReactionFromThought };
--- src/controllers/api/users.js
+++ src/controllers/api/users.js
@@ -28,11 +28,62 @@ const getUserById = (req, res) => {
 
 
 
-const createUser = (req, res) => {};
+const createUser = (req, res) => {
+    try {
+        const { userName, email } = req.body;
+    
+        if (userName && email) {
+          await User.create({ userName, email });
+          return res.json({ success: true });
+        } else {
+          return res.status(400).json({
+            success: false,
+            error: `Please enter valid username and email`,
+          });
+        }
+      } catch (error) {
+        console.log(`[ERROR]: could not create new user | ${error.message}`);
+        return res.status(500).json({ success: false, error: error.message });
+      }
+};
 
-const updateUser = (req, res) => {};
+const updateUser = (req, res) => {
+    try {
+        const { id } = req.params;
+        const { userName, email } = req.body;
+        if (userName || email) {
+          await User.findByIdAndUpdate(id, {
+            userName,
+            email,
+          });
+          return res.json({ success: true });
+        } else res.status(500).json({ success: false });
+      } catch (error) {
+        console.log(`[ERROR]: could not update user | ${error.message}`);
+        return res.status(500).json({ success: false, error: error.message });
+      }
+};
 
-const deleteUser = (req, res) => {};
+const deleteUser = (req, res) => {
+    const { id } = req.params;
+    try {
+      const user = await User.findById(id);
+      const userThoughts = user.thoughts;
+      console.log(userThoughts);
+      await Thoughts.deleteMany({ _id: userThoughts });
+  
+      try {
+        await User.findByIdAndDelete(id);
+        return res.json({ success: true });
+      } catch (error) {
+        console.log(`[ERROR]: could not delete user | ${error.message}`);
+        return res.status(500).json({ success: false, error: error.message });
+      }
+    } catch (error) {
+      console.log(`[Error]: could not get user | ${error.message}`);
+      return res.status(500).json({ success: false, error: error.message });
+    }
+};
 
 
 module.exports = {
