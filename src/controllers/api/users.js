86f752b4525af7fd58338027c886c26926f10192
Sophia Wall <sophiapwall@yahoo.co.uk>
2022-08-11 11:45:28 +0100

    async

--- src/controllers/api/users.js
+++ src/controllers/api/users.js
@@ -1,95 +1,92 @@
 const { User, Thought } = require("../../models");
 
-const getAllUsers = (req, res) => {  
-    try {
-        const users = await User.find({});
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find({});
 
-        return res.json({ data: users });
-        } catch (error) {
-        console.log(`[Error]: Failed to get all users | ${error.message}`);
-    }
+    return res.json({ data: users });
+  } catch (error) {
+    console.log(`[Error]: Failed to get all users | ${error.message}`);
+  }
 };
 
-const getUserById = (req, res) => {
-    try {
-        const { id } = req.params;
-    
-        const user = await User.findById(id);
+const getUserById = async (req, res) => {
+  try {
+    const { id } = req.params;
 
-        if (!user) {
-            return res.status(404).json({ success: false });
-        }
+    const user = await User.findById(id);
 
-        return res.json({ data: user});
-    } catch (error) {
-        console.log(`[Error]: Failed to get the user | ${error.message}`); 
+    if (!user) {
+      return res.status(404).json({ success: false });
     }
-}
 
-
+    return res.json({ data: user });
+  } catch (error) {
+    console.log(`[Error]: Failed to get the user | ${error.message}`);
+  }
+};
 
 const createUser = async (req, res) => {
-    try {
-        const { username, email } = req.body;
-    
-        if (username && email) {
-          await User.create({ userName, email });
-          return res.json({ success: true });
-        } else {
-          return res.status(400).json({
-            success: false,
-            error: `Please enter valid username and email`,
-          });
-        }
-      } catch (error) {
-        console.log(`[ERROR]: could not create new user | ${error.message}`);
-        return res.status(500).json({ success: false, error: error.message });
-      }
+  try {
+    const { username, email } = req.body;
+
+    if (username && email) {
+      await User.create({ userName, email });
+      return res.json({ success: true });
+    } else {
+      return res.status(400).json({
+        success: false,
+        error: `Please enter valid username and email`,
+      });
+    }
+  } catch (error) {
+    console.log(`[ERROR]: could not create new user | ${error.message}`);
+    return res.status(500).json({ success: false, error: error.message });
+  }
 };
 
 const updateUser = async (req, res) => {
-    try {
-        const { id } = req.params;
-        const { username, email } = req.body;
-        if (username || email) {
-          await User.findByIdAndUpdate(id, {
-            username,
-            email,
-          });
-          return res.json({ success: true });
-        } else res.status(500).json({ success: false });
-      } catch (error) {
-        console.log(`[ERROR]: could not update user | ${error.message}`);
-        return res.status(500).json({ success: false, error: error.message });
-      }
+  try {
+    const { id } = req.params;
+    const { username, email } = req.body;
+    if (username || email) {
+      await User.findByIdAndUpdate(id, {
+        username,
+        email,
+      });
+      return res.json({ success: true });
+    } else res.status(500).json({ success: false });
+  } catch (error) {
+    console.log(`[ERROR]: could not update user | ${error.message}`);
+    return res.status(500).json({ success: false, error: error.message });
+  }
 };
 
 const deleteUser = async (req, res) => {
-    const { id } = req.params;
+  const { id } = req.params;
+  try {
+    const user = await User.findById(id);
+    const userThoughts = user.thoughts;
+    console.log(userThoughts);
+    await Thought.deleteMany({ _id: userThoughts });
+
     try {
-      const user = await User.findById(id);
-      const userThoughts = user.thoughts;
-      console.log(userThoughts);
-      await Thought.deleteMany({ _id: userThoughts });
-  
-      try {
-        await User.findByIdAndDelete(id);
-        return res.json({ success: true });
-      } catch (error) {
-        console.log(`[ERROR]: could not delete user | ${error.message}`);
-        return res.status(500).json({ success: false, error: error.message });
-      }
+      await User.findByIdAndDelete(id);
+      return res.json({ success: true });
     } catch (error) {
-      console.log(`[Error]: could not get user | ${error.message}`);
+      console.log(`[ERROR]: could not delete user | ${error.message}`);
       return res.status(500).json({ success: false, error: error.message });
     }
+  } catch (error) {
+    console.log(`[Error]: could not get user | ${error.message}`);
+    return res.status(500).json({ success: false, error: error.message });
+  }
 };
 
-
 module.exports = {
-    getAllUsers,
-    getUserById,
-    createUser,
-    updateUser,
-    deleteUser,
-  };
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+};
--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -4,9 +4,11 @@ const router = Router();
 const friendsRoutes = require("./friends");
 const thoughtsRoutes = require("./thoughts");
 const reactionsRoutes = require("./reactions");
+const usersRouters = require("./users");
 
 router.use("/friends", friendsRoutes);
 router.use("/thoughts", thoughtsRoutes);
 router.use("/reactions", reactionsRoutes);
+router.use("/users", usersRouters);
 
 module.exports = router;
