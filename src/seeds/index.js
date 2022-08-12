cd22b742ccdeaae8ff7702e1a8ee28bb304a30a1
Sophia Wall <sophiapwall@yahoo.co.uk>
2022-08-12 11:34:22 +0100

    init function

---
+++ src/seeds/index.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+const { mongoose } = require("mongoose");
+const { Thought, User } = require("../models");
+const thought = require("./thought.json");
+const user = require("./user.json");
+
+const init = async () => {};
+
+init();
